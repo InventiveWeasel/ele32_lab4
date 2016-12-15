082414a1430ed36aaa6d3278055c226b5559761b
Gabriel Brito <gabriel.brito55@gmail.com>
2016-12-14 23:01:09 -0200

    Scrambler OK

--- src/ele_32_lab4/Scrambler.java
+++ src/ele_32_lab4/Scrambler.java
@@ -14,6 +14,9 @@ public class Scrambler {
 	}
 	
 	public int getBit(){
+		if(contador == 2000){
+			reset();
+		}
 		System.arraycopy(states, 0, auxStates, 0, states.length);
 		states[0] = (auxStates[7]+auxStates[4]+auxStates[2]+auxStates[0])%2;
 		for(int i = 1; i < states.length ; i++){
